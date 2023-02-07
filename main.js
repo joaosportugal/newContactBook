0e957949c24f6884bde9c38b8e2f53f5d29b40bd
Joao Portugal <joaosportugal@hotmail.com>
2023-02-06 21:50:59 -0300

    adicionada função de destacar os contatos já adicionados

--- main.js
+++ main.js
@@ -1,17 +1,70 @@
-// let names = [];
-// let numbers = [];
-
 function getValue (fieldName) {
     return document.getElementById(fieldName).value;
 }
 
-function run() {
-    const submitButton = document.getElementById('submitButton');
-    // const bodyTable = document.getElementById('bodyTable');
 
-    // function validate(name, number) {
+(function run() {
 
-    // }
+    let contacts = [];
+
+    function validate(contacts, formName, formNumber) {
+
+        const errors = [];
+        const bothEqual = ({name, number}) => name == formName && number == formNumber
+        const sameName = ({name, number}) => name == formName && number != formNumber
+        const sameNumber = ({name, number}) => name != formName && number == formNumber
+        const highlighted = document.querySelectorAll('.repeatedData');
+        highlighted.forEach(element => element.classList.remove('repeatedData'));
+
+        function highlightContact(caseOfRepeatedData) {
+            const indexOfRepeatedData = contacts.findIndex(caseOfRepeatedData);
+            const existingContact = document.querySelectorAll('.contact');
+            const repeatedData = existingContact[indexOfRepeatedData];
+            const repeatedName = repeatedData.querySelector('.name');
+            const repeatedNumber = repeatedData.querySelector('.number');
+
+            repeatedName.classList.remove('repeatedData');
+            repeatedNumber.classList.remove('repeatedData');
+
+            if (contacts.find(bothEqual)) {
+
+                repeatedName.classList.add('repeatedData');
+                repeatedNumber.classList.add('repeatedData');
+
+            } else if(contacts.find(sameName)) {
+
+                repeatedName.classList.add('repeatedData');
+                repeatedNumber.classList.remove('repeatedData');
+
+            } else if (contacts.find(sameNumber)) {
+
+                repeatedName.classList.remove('repeatedData');
+                repeatedNumber.classList.add('repeatedData');
+
+            }
+        }
+
+        if (contacts.find(bothEqual)) {
+            highlightContact(bothEqual);
+            errors.push('Esse contato já existe');
+        } else if(contacts.find(sameName)) {
+            highlightContact(sameName);
+            errors.push('Esse nome já existe');
+        } else if (contacts.find(sameNumber)) {
+            highlightContact(sameNumber);
+            errors.push('Esse número já existe');
+        }
+        return errors;
+        
+    }
+    
+    function loadContacts() {
+        contacts = [
+            {name: "João", number: "1111111"},
+            {name: "Bruno", number: "22222222"},
+            {name: "Paulo", number: "3333333"},
+        ];
+    }
 
     function renderTable(contacts) {
         const contactList = document.getElementById('bodyTable');
@@ -19,97 +72,42 @@ function run() {
         contacts.forEach(
             ({ name, number }) => {
                 contactList.innerHTML += 
-                    `<tr>
-                        <td>${name}</td>
-                        <td>${number}</td>
+                    `<tr class= "contact">
+                        <td class= "name">${name}</td>
+                        <td class= "number">${number}</td>
                     </tr>`;
             }
         )
     }
 
+    function addNewLine(contacts, name, number) {
+        const errorMessage = document.getElementById('errormessage');
+        errorMessage.style.display = 'none';
+        contacts.push({ name, number });
+        renderTable(contacts);
+    }
+
+    function showErrorMessages(validationResults) {
+        const errorMessage = document.getElementById('errormessage');
+        errorMessage.style.display = 'flex';
+        errorMessage.innerHTML = validationResults[0]
+    }
+
     function readFormAndAddNewLine(){
-        // const name = getValue('name');
-        // const number = getValue('number');
-        // const validationResults = validate(name, number);
-        // error = { fieldName: null, message: 'Esse contato já existe'}
-        // error = { fieldName: 'name', message: 'Já existe um contato com esse nome ....'}
-        // // fieldName:name|null message
-        // console.log({ name, number });
-        const contacts = [
-            {name: "João", number: "1111111"},
-            {name: "Bruno", number: "22222222"},
-            {name: "Paulo", number: "3333333"},
-        ];
-        renderTable(contacts)
+        const name = getValue('name');
+        const number = getValue('number');
+        const validationResults = validate(contacts, name, number);
+        const hasValidationError = validationResults.length > 0;
+        hasValidationError ? showErrorMessages(validationResults) : addNewLine(contacts, name, number)
+    }
 
+    function addSubmitButtonListener(contacts) {
+        const submitButton = document.getElementById('submitButton');
+        submitButton.addEventListener('click', readFormAndAddNewLine);
+    }
+
+    loadContacts();
+    renderTable(contacts);
+    addSubmitButtonListener();
     
-        // document.getElementById('name').value = "";
-        // document.getElementById('number').value = "";
-    }
-     
-    //console.log(submitButton);
-    submitButton.addEventListener('click', readFormAndAddNewLine)
-}
-
-run();
-
-
-
-
-
-
-
-
-
-
-function printContact(names, numbers) {
-    const contactList = document.getElementById('bodyTable');
-    contactList.innerHTML += `<tr>
-                        <td>${names[names.length - 1]}</td>
-                        <td>${numbers[numbers.length -1]}</td>
-                        </tr>`;
-    const lastAddedContactIndex = bodyTable.children.length;
-    const lastAddedContact = bodyTable.children[lastAddedContactIndex - 1];
-    lastAddedContact.classList.add('lastAddedContact'); 
-    const oneBeforeLastAddedContact = bodyTable.children[lastAddedContactIndex - 2];
-    if (oneBeforeLastAddedContact !== undefined) {
-        oneBeforeLastAddedContact.classList.remove('lastAddedContact');
-    }
-    return console.log(`O contato ${names[names.length - 1]} de número ${numbers[numbers.length -1]} foi adicionado com sucesso!`);
-}
-
-function getContact(names, numbers) {
-    // const name = document.getElementById('name');
-    // const number = document.getElementById('number');
-    const errorMessage = document.getElementById('errormessage');
-    switch(true) {
-        // contato igual
-        case names.includes(name.value) && numbers.includes(number.value):
-            console.log('Esse contato já existe');
-            errorMessage.style.display = 'flex';
-            errorMessage.innerHTML = `Esse contato já existe`;
-            break;
-        // nome igual e numero diferente
-        case names.includes(name.value) && !numbers.includes(number.value):
-            console.log(`Já existe um contato com esse nome ${name.value}`);
-            errorMessage.style.display = 'flex';
-            errorMessage.innerHTML = `Já existe um contato com esse nome ${name.value}`;
-            break;
-        // nome diferente e numero igual
-        case !names.includes(name.value) && numbers.includes(number.value):
-            console.log(`Já existe um contato com esse número ${number.value}`);
-            errorMessage.style.display = 'flex';
-            errorMessage.innerHTML = `Já existe um contato com esse número ${number.value}`
-            break;
-        // default
-        default :
-            names.push(`${name.value}`);
-            numbers.push(`${number.value}`);
-            printContact(names, numbers);
-            errorMessage.style.display = 'none';
-    }
-}
-
-
-
-
+})();
